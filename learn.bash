92657736a71e0435d6a7449f3223f58afd280bed
Openset <openset.wang@gmail.com>
2018-08-03 16:04:28 +0800

    Add: array

--- learn.bash
+++ learn.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Author: Openset
 # Home: https://github.com/openset
 
@@ -7,7 +7,7 @@
 # ------------------------------------------------------------------------
 # 以"#"开头的行就是注释，会被解释器忽略。
 # sh里没有多行注释，只能每一行加一个#号。
-# 多行注释
+# 多行注释(非语法)
 :<<EOF
 注释内容...
 注释内容...
@@ -54,9 +54,30 @@ greeting_2='hello, '$world' !'  # hello, world !
 greeting_3='hello, ${world} !'  # hello, ${world} !
 echo $greeting_2  $greeting_3
 # 获取字符串长度
-string="abcd"
-echo ${#string}     # 4
+string="Hello"
+echo ${#string}     # 5
 # 提取子字符串
 string="I love china!"
 echo ${string:0:4}  # I lo
+
+# ------------------------------------------------------------------------
+# 数组
+# ------------------------------------------------------------------------
+# 定义数组
+array_name=('value0' 'value1' 'value2' 'value3')
+# 读取数组
+# 使用 @ 符号可以获取数组中的所有元素
+echo ${array_name[@]}   # value0 value1 value2 value3
+echo ${array_name[0]}   # value0
+echo ${array_name[2]}   # value2
+# 获取数组的长度
+# 取得数组元素的个数
+length=${#array_name[@]}
+echo $length            # 4
+# 或者
+length=${#array_name[*]}
+echo $length            # 4
+# 取得数组单个元素的长度
+length=${#array_name[1]}
+echo $length            # 6
 
